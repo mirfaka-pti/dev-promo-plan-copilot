cbe3b100c2ce2a75cbf0b3498520e1e820128224
mirfaka-pti <achmad.mirfak@paracorpgroup.com>
2026-07-14 22:50:12 +0700

    initial commit

---
+++ taskpane.js
@@ -0,0 +1,51 @@
+Office.onReady(async () => {
+
+    // Workbook URL
+    const workbookUrl =
+        Office?.context?.document?.url || "Workbook belum tersimpan di OneDrive / SharePoint";
+
+    document.getElementById("workbookUrl").textContent =
+        workbookUrl;
+
+    // User Name
+    try {
+
+        const token =
+            await OfficeRuntime.auth.getAccessToken({
+                allowSignInPrompt: true,
+                allowConsentPrompt: true
+            });
+
+        const response = await fetch(
+            "https://graph.microsoft.com/v1.0/me",
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(
+                `Graph API Error (${response.status})`
+            );
+        }
+
+        const user = await response.json();
+
+        document.getElementById("userName").textContent =
+            user.displayName ||
+            user.userPrincipalName ||
+            "User tidak ditemukan";
+
+    } catch (error) {
+
+        console.error(error);
+
+        document.getElementById("userName").textContent =
+            "SSO belum dikonfigurasi";
+
+        document.getElementById("userName").classList.add("error");
+    }
+
+});
